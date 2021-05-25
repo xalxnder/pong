b2298a7474f92038ee6548e72613088368620321
Xavier Alexander <xalexander51@gmail.com>
2021-05-25 18:57:31 -0400

    Update

--- pong.py
+++ pong.py
@@ -5,38 +5,40 @@ import time
 screen = Screen()
 screen.screensize(800,800)
 screen.tracer(0)
-left_paddle = []
-right_paddle = []
-left_count = 0
-right_count = 0
-
-
-
 
 #Create The Left Paddle
-for i in range(4):
-	paddle = Turtle()
-	paddle.penup()
-	paddle.shape("square")
-	left_paddle.append(paddle)
-	paddle.setpos(-400, left_count)
-	left_count += 15
+left_paddle = Turtle()
+left_paddle.penup()
+left_paddle.setpos(-400,0)
+left_paddle.setheading(90)
+left_paddle.speed(10)
+left_paddle.color("blue")
+left_paddle.shape("square")
+left_paddle.shapesize(0.8, 4)
 
-#Create The Right Paddle
-for i in range(4):
-	paddle = Turtle()
-	paddle.penup()
-	paddle.shape("square")
-	left_paddle.append(paddle)
-	paddle.setpos(400,right_count)
-	right_count += 15
+
+def move_up():
+	left_paddle.setheading(90)
+	left_paddle.forward(20)
+
+
+def move_down():
+	left_paddle.setheading(270)
+	left_paddle.forward(20)
+
+screen.onkey(move_up, "Up")
+screen.onkey(move_down, "Down")
 
 
 ball = Turtle()
+ball.penup()
 ball.shape("circle")
-screen.listen()
-time.sleep(0.1)
-screen.update()
+
+game_on = True
+while game_on:
+	screen.listen()
+	# time.sleep(0.1)
+	screen.update()
 
 
 
