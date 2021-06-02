fb03c1931ce9187ca18d00c34e7f7a41cc7736e7
Xavier Alexander <xalexander51@gmail.com>
2021-06-02 12:10:33 -0400

    Remove cpu and add 2nd player.

--- pong.py
+++ pong.py
@@ -7,78 +7,93 @@ screen.setup(900,700)
 screen.tracer(0)
 
 #Create The Players Paddle
-player_paddle = Turtle()
-player_paddle.penup()
-player_paddle.setpos(-400,0)
-player_paddle.setheading(90)
-player_paddle.speed(10)
-player_paddle.color("blue")
-player_paddle.shape("square")
-player_paddle.shapesize(0.8, 4)
+left_paddle = Turtle()
+left_paddle.penup()
+left_paddle.setpos(-400,0)
+left_paddle.setheading(90)
+left_paddle.speed(0)
+left_paddle.color("blue")
+left_paddle.shape("square")
+left_paddle.shapesize(0.8, 4)
+
+#Create The Players Paddle
+right_paddle = Turtle()
+right_paddle.penup()
+right_paddle.setpos(400,0)
+right_paddle.setheading(90)
+right_paddle.speed(0)
+right_paddle.color("blue")
+right_paddle.shape("square")
+right_paddle.shapesize(0.8, 4)
 
 
-#Create the Computers Paddle
-cpu_paddle = Turtle()
-cpu_paddle.penup()
-cpu_paddle.setpos(400,0)
-cpu_paddle.setheading(90)
-cpu_paddle.speed(10)
-cpu_paddle.color("blue")
-cpu_paddle.shape("square")
-cpu_paddle.shapesize(0.8, 4)
-
-
-def move_up():
-	if player_paddle.ycor() < 290:
-		player_paddle.setheading(90)
-		player_paddle.forward(20)
-
-
-def move_down():
-	if player_paddle.ycor() > -290:
-		player_paddle.setheading(270)
-		player_paddle.forward(20)
-
-
-def cpu_move():
-	cpu_paddle.forward(20)
-	if cpu_paddle.ycor() == 300:
-		cpu_paddle.setheading(270)
-	elif cpu_paddle.ycor() == -300:
-		cpu_paddle.setheading(90)
-
-
-ball_x_move = -10
-ball_y_move = -10
-
-def move_ball():
-	ball.goto((ball.xcor() + ball_x_move), (ball.ycor() + ball_y_move))
-
-screen.onkey(move_up, "Up")
-
-screen.onkey(move_down, "Down")
 
 ball = Turtle()
 ball.penup()
 ball.shape("circle")
 ball.speed(8)
 
+def left_move_up():
+	if left_paddle.ycor() < 290:
+		left_paddle.setheading(90)
+		left_paddle.forward(20)
+
+
+def left_move_down():
+	if left_paddle.ycor() > -290:
+		left_paddle.setheading(270)
+		left_paddle.forward(20)
+
+
+def right_move_up():
+	if right_paddle.ycor() < 290:
+		right_paddle.setheading(90)
+		right_paddle.forward(20)
+
+
+def right_move_down():
+	if right_paddle.ycor() > -290:
+		right_paddle.setheading(270)
+		right_paddle.forward(20)
+
+
+
+ball_x_move = -10
+ball_y_move = -10
+
+
+def move_ball():
+	ball.goto((ball.xcor() + ball_x_move), (ball.ycor() + ball_y_move))
+
+
+screen.listen()
+#Player 1
+
+
+#Player 2
+screen.onkey(right_move_up, "Up")
+screen.onkey(right_move_down, "Down")
+screen.onkey(left_move_up, "w")
+screen.onkey(left_move_down, "s")
+
 game_on = True
 
 while game_on:
+	screen.delay(30)
 	screen.update()
 	move_ball()
-	cpu_move()
 	if ball.ycor() >= 330 or ball.ycor() <= -330:
 		ball_y_move *= -1
-		print(ball_y_move)
 
-	if ball.distance(player_paddle) < 40:
+	if ball.distance(left_paddle) < 50:
+		ball_x_move *= -1
+		print("ouch")
+
+	if ball.distance(right_paddle) < 50:
 		ball_x_move *= -1
 		print("ouch")
 
 
-	screen.listen()
 	time.sleep(0.1)
 
 
